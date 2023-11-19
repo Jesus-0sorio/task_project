dc4ba63f94f8aac81ae1aa0ba54c6b2842ff2b5d
Jesus-0sorio <jaoz10.16@gmail.com>
2023-11-18 21:44:39 -0500

    Add console.log for BASE_URL

--- Frontend/src/config/axios.js
+++ Frontend/src/config/axios.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 
+console.log('BASE_URL', BASE_URL);
+
 const token = localStorage.getItem('token');
 
 export const api = axios.create({
